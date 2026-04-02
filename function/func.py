44e5ce2d546b447738f6ff0fdd716f93ca6174d1
firmanramadan97 <firmanramadan97@gmail.com>
2026-04-02 14:41:36 +0700

    add func

--- function/func.py
+++ function/func.py
@@ -1,22 +1,6 @@
 import streamlit as st
 import time
 
-# def check_login(username, password):
-#     try:
-#         # Panggil data tanpa memicu spinner global
-#         df = conn.read(worksheet="akun", ttl=0)
-#         df.columns = df.columns.str.strip().str.lower()
-#         user_match = df[(df['username'].astype(str) == str(username).strip()) & 
-#                         (df['password'].astype(str) == str(password).strip())]
-#         if not user_match.empty:
-#             return user_match.iloc[0]['name']
-#     except Exception as e:
-#         # Opsional: Log error ke console untuk debugging
-#         print(f"DEBUG: Error reading sheets: {e}")
-        
-#     return None
-
-
 
 def check_login(conn, username, password):
     """
